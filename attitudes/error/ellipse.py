a80a06d91f103c3809931ce6f11e1384713a8f23
Daven Quinn <Daven@Davens-MacBook-Pro.local>
2014-01-28 01:13:12 -0800

    Got rid of functions to compute Euler angles
    We can always bring them back if needed.

--- attitudes/error/ellipse.py
+++ attitudes/error/ellipse.py
@@ -1,18 +1,5 @@
-from __future__ import print_function
+from __future__ import division
 import numpy as N
-
-def euler_angles(rotation_matrix):
-    """ Compute Euler angles from rotation matrix"""
-    r = rotation_matrix
-    if r.shape[1] == 2:
-        angles = [N.arctan2(r[1,0], r[0,0]),
-                  N.pi/2-N.arctan2(r[1,0],-r[0,0])]
-        return N.array(angles)
-
-    return [N.arctan2(r[2,1],r[2,2]),
-              N.arctan2(-r[2, 0], N.sqrt(r[0,0]**2 + r[1,0]**2)),
-              N.arctan2(r[1, 0],r[0, 0])]
-
 
 def ellipse(center,covariance_matrix):
     """Returns error ellipse in slope-azimuth space"""
@@ -20,10 +7,6 @@ def ellipse(center,covariance_matrix):
     U, s, rotation_matrix = N.linalg.svd(covariance_matrix)
     # semi-axes (largest first)
     saxes = N.sqrt(s)
-    volume = N.pi*N.prod(saxes)
-    # rotation matrix
-    angles = euler_angles(rotation_matrix)
-
     u = N.linspace(0, 2*N.pi, 100)
     data = N.column_stack((saxes[0]*N.cos(u), saxes[1]*N.sin(u)))
     # rotate data
