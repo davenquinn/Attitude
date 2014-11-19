0689b98d160da488bc6fc269ed106016080be4a9
Daven Quinn <dev@davenquinn.com>
2014-11-19 00:31:09 -0800

    Removed a duplicate function

--- attitude/plot/__init__.py
+++ attitude/plot/__init__.py
@@ -17,23 +17,7 @@ def trend_plunge(orientation, *args, **kwargs):
 		el = map(N.degrees,orientation.error_ellipse(vector=True, level=level))
 		lat,lon = line(el[1], el[0])
 		e = Polygon(zip(lat,lon), alpha=a[i], **kwargs)
-		ax.add_artist(e)
-
-def strike_dip(orientation, *args, **kwargs):
-	ax = kwargs.pop("ax",P.gca())
-	levels = kwargs.pop("levels",[1])
-	kwargs["linewidth"] = 0
-
-	a = kwargs.pop("alpha",0.7)
-	if len(a) != len(levels):
-		a = [a]*len(levels)
-
-	for i,level in enumerate(levels):
-		print level
-		el = map(N.degrees,orientation.error_ellipse(level=level))
-		e = Polygon(zip(*el), alpha=a[i], **kwargs)
 		ax.add_patch(e)
-
 
 def normal(orientation, *args, **kwargs):
 	ax = kwargs.pop("ax",P.gca())
@@ -48,7 +32,7 @@ def normal(orientation, *args, **kwargs):
 		el = map(N.degrees,orientation.error_ellipse(vector=True, level=level))
 		lat,lon = line(90-el[1], 180+el[0])
 		e = Polygon(zip(lat,lon), alpha=a[i], **kwargs)
-		ax.add_artist(e)
+		ax.add_patch(e)
 
 def strike_dip(orientation, *args, **kwargs):
 	ax = kwargs.pop("ax",P.gca())
