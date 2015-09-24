4fa8760594dbcf97f70df88fbba1459b02d8bbbc
Daven Quinn <dev@davenquinn.com>
2015-09-24 12:56:32 -0700

    Wrapped everything in function

--- test-conic.py
+++ test-conic.py
@@ -75,99 +75,104 @@ def pole(conic, plane):
     v = dot(N.linalg.inv(conic),plane)
     return v[:3]/v[3]
 
-# We consider a sphere with radius 1 offset 2 units on the X axis
-# the half-angle of its shadow will be sin(theta) = 1/2, or theta = 30º
-# Can we recreate this?
+def run_tests():
+    # We consider a sphere with radius 1 offset 2 units on the X axis
+    # the half-angle of its shadow will be sin(theta) = 1/2, or theta = 30º
+    # Can we recreate this?
 
-r = 1
-offs = 2
+    r = 1
+    offs = 2
 
-# Ellipsoid
-ell0 = N.identity(4)
-ell0[3,3] = -1
+    # Ellipsoid
+    ell0 = N.identity(4)
+    ell0[3,3] = -1
 
-assert same(center(ell0),[0,0,0])
+    assert same(center(ell0),[0,0,0])
 
-# Translation matrix
-T = N.identity(4)
-T[0,3] = 2
-ell = dot(T.T,ell0,T)
+    # Translation matrix
+    T = N.identity(4)
+    T[0,3] = 2
+    ell = dot(T.T,ell0,T)
 
-assert same(center(ell),[2,0,0])
+    assert same(center(ell),[2,0,0])
 
-# Check that translation is reversible
-T[0,3] = -2
-assert same(ell0, dot(T.T,ell,T))
+    # Check that translation is reversible
+    T[0,3] = -2
+    assert same(ell0, dot(T.T,ell,T))
 
-# Check that we have an ellipsoid
-assert N.linalg.det(ell[:3,:3]) > 0
+    # Check that we have an ellipsoid
+    assert N.linalg.det(ell[:3,:3]) > 0
 
-# Plane of tangency
-origin = point(0,0,0)
-# equation of plane polar to origin
-plane = polar(ell,origin)
+    # Plane of tangency
+    origin = point(0,0,0)
+    # equation of plane polar to origin
+    plane = polar(ell,origin)
 
-# distance from tangent plane to origin
-hn = hessian_normal(plane)
-assert hn[3] == 1.5
+    # distance from tangent plane to origin
+    hn = hessian_normal(plane)
+    assert hn[3] == 1.5
 
-# pole of tangent plane?
-assert same(origin, pole(ell,plane))
+    # pole of tangent plane?
+    assert same(origin, pole(ell,plane))
 
-# center is inside ellipsoid
-assert inside(ell,center(ell))
-# origin is outside of ellipsoid
-assert not inside(ell,origin)
+    # center is inside ellipsoid
+    assert inside(ell,center(ell))
+    # origin is outside of ellipsoid
+    assert not inside(ell,origin)
 
-n = hn[:3]
-# point in plane
-pt = hn[3]*n
-# Get two vectors in plane
-v1 = N.cross(n,[0,1,0])
-v2 = N.cross(v1,n)
-
-
-# plane can be represented
-#1x3 3x4
-# a v1 + b v2 + x_0 = x
-#a v1 + b v2 = x_c
-# [a b 1] [v1 v2 x0] = x
-# 4x3 3x1
-# a p = x
-# a = p^-1 x
-
-# transformation matrix
-m = N.column_stack((v1,v2,pt))
-m = N.append(m,N.array([[0,0,1]]),axis=0)
-
-con = dot(m.T, ell, m)
-
-# |a|
-# |b| [ d e f ] = ad + be + cf
-# |c|
-
-#e = dot(p.T,ell,p)
-#from scipy.linalg import lu
-#L,U = lu(e,permute_l=True)
-
-offs = N.array([0,0,1])
-
-# X^T L = 0
+    n = hn[:3]
+    # point in plane
+    pt = hn[3]*n
+    # Get two vectors in plane
+    v1 = N.cross(n,[0,1,0])
+    v2 = N.cross(v1,n)
 
 
-# X^T L = 0
-# U X = 0
+    # plane can be represented
+    #1x3 3x4
+    # a v1 + b v2 + x_0 = x
+    #a v1 + b v2 = x_c
+    # [a b 1] [v1 v2 x0] = x
+    # 4x3 3x1
+    # a p = x
+    # a = p^-1 x
 
-# T^T m^T L = 0
-# U m T = 0e
+    # transformation matrix
+    m = N.column_stack((v1,v2,pt))
+    m = N.append(m,N.array([[0,0,1]]),axis=0)
 
-# Cone of tangency
-# equation of elliptic cone
-B = N.sqrt(3)/2 # cos(30º)
-cone = N.diag([-1.5,B,B,0])
+    con = dot(m.T, ell, m)
 
-assert N.arctan(B/1.5) == N.radians(30)
+    # |a|
+    # |b| [ d e f ] = ad + be + cf
+    # |c|
 
-assert same(center(cone),origin)
+    #e = dot(p.T,ell,p)
+    #from scipy.linalg import lu
+    #L,U = lu(e,permute_l=True)
 
-import IPython; IPython.embed()
+    offs = N.array([0,0,1])
+
+    # X^T L = 0
+
+
+    # X^T L = 0
+    # U X = 0
+
+    # T^T m^T L = 0
+    # U m T = 0e
+
+    # Cone of tangency
+    # equation of elliptic cone
+    B = N.sqrt(3)/2 # cos(30º)
+    cone = N.diag([-1.5,B,B,0])
+
+    assert N.arctan(B/1.5) == N.radians(30)
+
+    assert same(center(cone),origin)
+
+try:
+    run_tests()
+except AssertionError as err:
+    print err
+    import IPython; IPython.embed()
