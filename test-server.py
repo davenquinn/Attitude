11e97c31dcfc16ec019b12bb11b5e69843bf7ad5
Daven Quinn <dev@davenquinn.com>
2015-09-28 11:58:57 -0700

    Added simple cases to test server

--- test-server.py
+++ test-server.py
@@ -7,6 +7,7 @@ test = Flask(__name__)
 from syrtis.core import app, db
 from syrtis.models import Attitude
 from attitude.display import env, report
+from attitude.orientation.tests import test_cases
 
 @test.route("/")
 def list():
@@ -15,13 +16,18 @@ def list():
     """
     with app.app_context():
         measurements = db.session.query(Attitude.id).all()
+        measurements += test_cases
         t = env.get_template("list.html")
         return t.render(measurements=measurements)
 
 @test.route("/<id>/")
 def measurement(id):
     with app.app_context():
-        measurement = db.session.query(Attitude).get(id)
+        try:
+            measurement = db.session.query(Attitude).get(id)
+        except Exception:
+            _ = [i for i in test_cases if i.id == id]
+            measurement = _[0]
         return report(measurement.array,name=id)
 
 test.run(debug=True)
