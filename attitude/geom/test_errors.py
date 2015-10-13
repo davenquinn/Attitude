7bfcbcb364536f289d827f6897d762d17cba9f66
Daven Quinn <dev@davenquinn.com>
2015-10-13 14:28:30 -0700

    Added method for slicing via plane

--- attitude/geom/test_errors.py
+++ attitude/geom/test_errors.py
@@ -29,29 +29,13 @@ def test_conic_errors():
     assert same(p.normal(),n)
     assert same(p.offset(),vector(0,0,0))
 
-    n = p.normal()
-    # Two vectors in plane
-    # Perhaps need to add a case for when
-    # plane is perpendicular to this vector
-    v1 = N.cross(n,vector(0,1,0))
-    if N.linalg.norm(v1) == 0:
-        # we need to use another axis
-        v1 = N.cross(n,vector(0,0,1))
-    v2 = N.cross(v1,n)
-    axes = (v1,v2)
-    pt = p.offset()
+    h1, rotation, offset = hyp.slice(p)
 
-    m = N.append(
-        N.column_stack((axes[0],axes[1],pt)),
-        N.array([[0,0,1]]),axis=0)
-
-    h1 = hyp.transform(m)
     assert h1.is_hyperbolic()
     d = N.abs(N.diagonal(h1)[:-1])
     axes = N.sqrt(1/d)
     assert same(axes,N.array([500,2]))
 
-    #y = lambda x: axes[1]*N.tan(N.arccos(axes[0]/x))
     u = lambda x: N.arcsinh(x/axes[0])
     y = lambda x: axes[1]*N.cosh(u(x))
     assert y(0) == axes[1]
