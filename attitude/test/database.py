feeacb7222f83e9168c0c5a5873915f43a3f98dc
Daven Quinn <dev@davenquinn.com>
2016-06-12 22:38:44 -0700

    Added test fixtures to testing infrastructure

--- attitude/test/database.py
+++
@@ -1,9 +0,0 @@
-from dataset import connect
-
-db = connect('sqlite:///:memory:')
-
-table = db['test_cases']
-
-table.insert(dict(name='test-1'))
-
-
---
+++ attitude/test/io.py
@@ -0,0 +1,52 @@
+"""
+Methods for dealing with saved test data
+"""
+from __future__ import print_function
+
+from os import path
+import numpy as N
+from cStringIO import StringIO
+
+__dirname__ = path.dirname(__file__)
+
+def __test_fp(name):
+    fn = path.basename(name)+'.txt'
+    return path.join(__dirname__,'fixtures',fn)
+
+def save_test_plane(name,*arrays):
+    """
+    Saves numpy array(s) defining plane data
+    to text file in `test/fixtures` directory.
+    This can be used to add data for inclusion
+    in the testing framework for this module.
+
+    Multiple arrays (for grouping, etc.) will
+    be saved to the same file.
+    """
+    with open(__test_fp(name),'w') as f:
+        for i,arr in enumerate(arrays):
+            N.savetxt(f, arr)
+            if i != len(arrays)-1:
+                print('---', file=f)
+
+def __split_text(f):
+    acc = ""
+    for line in f:
+        if line.strip() == '---':
+            yield acc
+            acc = ""
+        else:
+            acc += line
+    yield acc
+
+def load_test_plane(name):
+    """
+    Load numpy array(s) defining plane data
+    from text file in `test/fixtures` directory
+    """
+    with open(__test_fp(name),'r') as f:
+        splits = (StringIO(txt) for txt in __split_text(f))
+        arrays = [N.loadtxt(fobj) for fobj in splits]
+        if len(arrays) == 1:
+            arrays = array[0]
+    return arrays
