6c161ec8418b76d28a1428eabc6eb08ee68957a7
Daven Quinn <dev@davenquinn.com>
2016-11-19 23:40:48 -0800

    Added methods to create hyperbolic axes

--- attitude/error/axes.py
+++ attitude/error/axes.py
@@ -4,6 +4,7 @@ and hyperbolic axes. These all operate in axis-aligned
 space---rotation into global coordinate systems should
 occur after these transformations are applied.
 """
+import numpy as N
 from scipy.stats import chi2
 
 def sampling_covariance(fit):
@@ -31,3 +32,19 @@ def sampling_axes(fit, confidence_level=0.95):
     sigma = chi2.ppf(confidence_level,fit.n-3)/fit.n
     return apply_error_level(cov,sigma)
 
+def __angular_error(hyp_axes, axis_length):
+    """
+    The angular error for an in-plane axis of
+    given length (either a PCA major axis or
+    an intermediate direction).
+    """
+    return N.arctan2(hyp_axes[-1],axis_length)
+
+def angular_errors(hyp_axes):
+    """
+    Minimum and maximum angular errors
+    corresponding to 1st and 2nd axes
+    of PCA distribution.
+    """
+    return tuple(__angular_error(hyp_axes, i)
+            for i in hyp_axes[:-1])
