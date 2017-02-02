a29aab28bd23f8ae119e5554f1b8de518f007eb2
Daven Quinn <dev@davenquinn.com>
2017-02-02 02:13:36 -0800

    At least add everything

--- attitude/plot/parametric.py
+++ attitude/plot/parametric.py
@@ -1,4 +1,4 @@
-import numpy as N
+from __future__ import division
 """
 Functions to produce parametric representations of
 conic sections in two dimensions. This can be applied
@@ -7,6 +7,8 @@ to plotting 2D slices of quadrics as well.
 We could generalize this to n dimensions, but that is probably
 not super useful at the moment.
 """
+import numpy as N
+from matplotlib import pyplot as P
 
 defaults = dict(
     center=N.zeros(2),
@@ -40,3 +42,52 @@ def ellipse(axes,  **kwargs):
         axes[0]*N.cos(th)+center[0],
         axes[1]*N.sin(th)+center[1])
 
+def plot_conjugate_conics(ax, axes):
+    hyp, = ax.plot(*hyperbola(axes))
+    ax.plot(*ellipse(axes), zorder=-5)
+
+    # Plot hyperbola focus
+    hyp_c = N.sqrt(N.sum(axes**2))
+    ax.plot([0,0],[hyp_c,-hyp_c], '.', color=hyp.get_color())
+
+    # Normal vector ellipse axes lengths
+    # scales inversely with axes but can
+    # be rescaled at will
+
+    ax1 = axes.copy()
+    ax1[0] = (axes[0]+axes[1]**2)/axes[0]
+
+    # Plot ellipse foci
+    c = ax1**2
+    c.sort()
+    c[0] *= -1
+    ell_c = N.sqrt(N.sum(c))
+
+    center = N.sqrt(hyp_c**2+ell_c**2)/N.sqrt(2)
+    #center=	axes[1]+ell_c#+axes[1]/axes[0]
+
+    ell, = ax.plot(*ellipse(ax1, center=[0,center]))
+
+    # Plot ellipse foci
+    ax.plot([0,0],[center+ell_c,center-ell_c], '.', color=ell.get_color())
+
+    # Plot tangents
+    xvals = N.array([-500,500])
+    yvals = axes[1]/axes[0]*xvals
+    kw = dict(zorder=-1, color=hyp.get_color(), linewidth=0.5)
+    ax.plot(xvals, yvals, ':', **kw)
+    ax.plot(xvals, -yvals, ':', **kw)
+
+    #yvals = axes[0]/axes[1]*xvals
+    kw = dict(zorder=-1, color=ell.get_color(), linewidth=0.5)
+    ax.plot(yvals, xvals, ':', **kw)
+    ax.plot(yvals, -xvals, ':', **kw)
+
+    m = N.linalg.norm(axes)*2
+    lim = N.array([-m,m])
+
+    ax.set_xlim(lim)
+    ax.set_ylim(lim*0.6)
+    ax.set_aspect(1)
+
+    return ax
