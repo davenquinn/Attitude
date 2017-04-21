ecc815950c2e150f679877cb96aeea37e13f42e9
Daven Quinn <dev@davenquinn.com>
2017-04-21 13:55:16 -0700

    Added parametric calculation of hyperbolas

--- attitude/display/hyperbola.py
+++ attitude/display/hyperbola.py
@@ -92,7 +92,86 @@ def hyperbolic_errors(hyp_axes, xvals,
     # Btm
     vals[:,-1] *= -1
     b = dot(vals,ax1).T+means[:,N.newaxis]
+
+    nom = dot(nom,ax1).T+means[:,N.newaxis]
+    return nom, b, t[:,::-1]
+
+def hyperbolic_errors_parametric(hyp_axes, xvals=None,
+    transformation=None, axes=None, means=None):
+
+    if means is None:
+        means = N.array([0,0])
+
+    arr = augment_tensor(N.diag(hyp_axes))
+
+    # Transform ellipsoid to dual hyperboloid
+    hyp = conic(arr).dual()
+
+    if len(hyp_axes) == 3:
+        # Three_dimensional case
+
+        if transformation is None:
+            transformation = N.identity(3)
+        if axes is None:
+            axes = N.array([[0,1,0],[0,0,1]])
+
+        hyp = hyp.transform(augment_tensor(transformation))
+
+        n_ = N.cross(axes[0],axes[1])
+
+        # Create a plane containing the two axes specified
+        # in the function call
+        p = plane(n_) # no offset (goes through origin)
+        h1 = hyp.slice(p, axes=axes)[0]
+    else:
+        # We have a 2d geometry
+        h1 = hyp
+
+    # Major axes of the conic sliced in the requested viewing
+    # geometry
+    H = N.sqrt(h1.semiaxes())
+
+    assert len(H) == 2
+
+    b,a = tuple(H[::-1])
+    cut_angle = N.arctan2(b,a)
+
+    #cut_angle += 0.00001 # For cleanliness
+    angles = N.linspace(cut_angle, N.pi-cut_angle, 500)
+    top = angles-N.pi/2
+    bottom = (angles+N.pi/2)[::-1]
+    # Apparent dip angle correction
+    a1 = axes[0].copy()
+    a1[-1] = 0
+    corr = angle(axes[0],a1)
+    if a < 1e-5:
+        a = 0
+
+    large_number = 10e5
+
+    def do(angles):
+        #corr = N.array([N.cos(
+        vals = N.array([N.tan(angles),1/N.cos(angles)])
+        arr = H*vals.T
+
+        sgn = N.sign(arr[0,1])
+        _ = b/a*large_number*sgn
+        start = N.array([-sgn*large_number,_])
+        end = N.array([sgn*large_number,_])
+        arr = N.vstack((start,arr,end))
+
+        corr = [N.cos(a),N.sin(a)]
+
+        return (arr*corr+means).T
+
+    b,t = tuple(do(_) for _ in [bottom, top])
+
+    nom = N.array([[N.cos(a), N.sin(a)]])
+
+    xvals = N.array([-500,500])
+    nom = N.array([xvals,N.zeros(2)]).transpose()
     return nom, b, t
+
 
 def hyperbola_values(hyp_axes, xvals):
     """
@@ -140,13 +219,15 @@ class HyperbolicErrors(object):
         self.poly = ax.add_patch(patch)
         if self.data is not None:
             self.__set_plot_data(self.data)
-        # Support method chaining
-        return self
+
+    def plot_nominal(self, ax, *args, **kw):
+        ax.plot(*self.data[0],*args, **kw)
 
     def update_data(self, *args, **kwargs):
-        self.data = hyperbolic_errors(*args,**kwargs)
+        self.data = hyperbolic_errors_parametric(*args,**kwargs)
         self.__set_plot_data(self.data)
 
     def __set_plot_data(self,n):
-        coords = N.concatenate((n[1],n[2][:,::-1]),axis=1).T
+        coords = N.concatenate((n[1],n[2]),axis=1).T
         self.poly.set_xy(coords)
+
