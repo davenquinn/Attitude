bb54ad279bf34ac1c904952747c63ecc756cd389
Daven Quinn <dev@davenquinn.com>
2017-12-21 00:55:09 -0500

    Modified stuff

--- js-frontend/rollup.config.js
+++ js-frontend/rollup.config.js
@@ -6,7 +6,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 let plugins = [
   coffee2({version: 2}),
-  stylus({output: 'styles.css'}),
+  stylus({output: 'lib/styles.css'}),
   nodeResolve({jsnext:true,main:true}),
   commonJS({extensions: [ '.js', '.coffee' ]})
 ];
