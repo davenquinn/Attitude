8cbc0a8a3bca7434161be26e0b1fca6accca1a2b
Daven Quinn <dev@davenquinn.com>
2018-01-24 19:54:59 -0800

    Updated UI example

--- ui-test/process-onis-planes.py
+++ ui-test/process-onis-planes.py
@@ -5,16 +5,35 @@ from attitude import Orientation, ReconstructedPlane, create_groups
 from json import dumps
 from sys import argv
 
-fn = path.join(environ["DATA"],'elevations.dxf')
+DATA = environ['DATA']
+
+fn = path.join(DATA,'elevations_v2.dxf')
 
 orientations = []
 with fiona.open(fn) as ds:
     for i,item in ds.items():
         coords = N.array(item['geometry']['coordinates'])
-        orientations.append(Orientation(coords))
+        try:
+            orientations.append(Orientation(coords))
+        except ValueError:
+            continue
 
 groups = (
+    ["3df53944","e98e12a9"],
+    ["493a240a","04987fe8"],
+    ["8565a276","939d9b9a"],
+    ["c2122b2a","de785eb5"],
+    ["730a6124","eb31c4b0"]
 )
+
+groupedOrientations = create_groups(orientations, *groups,
+                             same_plane=False)
+
+disabled = ["afb1b38f","ebab5ecb","7b12f5d4","72417f52"]
+groupedOrientations = [o for o in groupedOrientations if o.hash not in disabled]
+
+collection = [a.to_mapping(color='#ff0000', type='remote')
+              for a in groupedOrientations]
 
 orientations = create_groups(orientations, *groups,
                              same_plane=False)
@@ -22,7 +41,7 @@ orientations = create_groups(orientations, *groups,
 collection = [a.to_mapping(color='#ff0000', type='remote')
               for a in orientations]
 
-fn = path.join(environ["DATA"],'field-orientations.geojson')
+fn = path.join(DATA,'field-orientations.geojson')
 
 with fiona.open(fn) as ds:
     for i,item in ds.items():
@@ -41,6 +60,8 @@ with fiona.open(fn) as ds:
             center=center,
             color='#444', type='in-situ')
         collection.append(orientation)
+
+collection = [c for c in collection if 1600 < c['center'][2] < 1680]
 
 output = dumps(collection,indent=4, sort_keys=True)
 try:
