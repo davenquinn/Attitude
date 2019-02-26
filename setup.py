fcfdf5d02997fbc5c54c90c7a5f6b48c5cfbb7be
Daven Quinn <dev@davenquinn.com>
2019-02-25 18:36:22 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setup(
     name='Attitude',
-    version="0.2.0",
+    version="0.3.0",
     description="Attitude computes the orientations of planes from point data.",
     license='MIT',
     keywords='gis data computation fitting statistics vector science geology',
