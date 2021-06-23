bc64e8740075af2ec0ee4cfe55b42a4be8711b98
Daven Quinn <dev@davenquinn.com>
2021-06-22 23:34:14 -0500

    Partial attempt to configure multipackage architecture

--- frontend/packages/webapp/index.ts
+++ frontend/packages/webapp/index.ts
@@ -1,5 +1,5 @@
-import h from "@macrostrat/hyper"
+import h from "@macrostrat/hyper";
 import { render } from "react-dom";
-import {AttitudeUI} from "../../js-frontend/ui-src/index.coffee"
+import { AttitudeUI } from "attitude-notebook-ui/src/index.coffee";
 
 render(h(AttitudeUI), document.getElementById("root"));
